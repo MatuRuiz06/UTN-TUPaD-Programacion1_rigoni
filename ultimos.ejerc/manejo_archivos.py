c5092ceec271a2983a2aee9fdcfdbda023a2e332
MatuRuiz06 <matiasnicolasruiz40@gmail.com>
2025-11-03 03:00:14 -0300

    Ultima practica real.

--- ultimos.ejerc/manejo_archivos.py
+++ ultimos.ejerc/manejo_archivos.py
@@ -1,8 +1,12 @@
-opcion = input("Ingrese la opción deseada: ")
+print("\n--- Manejo de Archivos: Registro de Productos ---\n")
+with open("productos.txt", "w", encoding="utf-8") as archivo:
+    archivo.write("Producto,Precio,Cantidad\n")
+    archivo.write("Lapicera | Precio: $120.5 | Cantidad: 30\n")
+opcion = str(input("¿Desea ingresar productos al archivo 'productos.txt'? (si/no): ")).lower()
 bucle = True
 while bucle:
     if opcion == "si":
-        with open("productos.txt", "w", encoding="utf-8") as archivo:
+        with open("productos.txt", "a", encoding="utf-8") as archivo:
             cantidad = int(input("¿Cuántos productos desea ingresar? "))
             for i in range(cantidad):
                 producto = str(input(f"Ingrese el nombre del producto {i+1}: ")).lower()
