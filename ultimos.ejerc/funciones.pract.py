df6efc0294aa4b1270fc3a8162ad64e7b40fcaf1
MatuRuiz06 <matiasnicolasruiz40@gmail.com>
2025-11-02 21:13:06 -0300

    Implementar función para calcular el promedio de tres notas y validar el rango de entrada

--- ultimos.ejerc/funciones.pract.py
+++ ultimos.ejerc/funciones.pract.py
@@ -1,8 +1,14 @@
-def calcular_imc(peso, altura):
-    IMC = float(peso/(altura**2))
-    return IMC
+def calcular_promedio(prom1, prom2, prom3):
+    prom_final = float((prom1 + prom2 + prom3)/3)
+    return prom_final
 
-peso = float(input("Ingrese su peso corporal en kilogramos: "))
-altura = float(input("Ingrese su altura en metros: "))
-IMC = calcular_imc(peso, altura)
-print(f"Su Indice de Masa Corporal (IMC) es de: {IMC}")
+promedio1 = float(input("Ingrese el primer promedio: "))
+promedio2 = float(input("Ingrese el segundo promedio: "))
+promedio3 = float(input("Ingrese el tercer promedio: "))
+if 0 <= promedio1 <= 10 and 0 <= promedio2 <= 10 and 0 <= promedio3 <= 10:
+    prom_final = calcular_promedio(promedio1, promedio2, promedio3)
+    print(f"El promedio final de los 3 promedios ingresados es de: {prom_final}")
+    print(f"Mientras que el mismo pero redondeado sería de: {round(prom_final)}")
+    print("\nGracias!")
+else:
+    print("ERROR: Ingresó un número fuera del rango de notas.")
