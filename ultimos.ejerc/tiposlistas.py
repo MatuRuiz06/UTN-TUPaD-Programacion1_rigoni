f3d6b2610bdb3a972e9440084afcd536ce187fcf
MatuRuiz06 <matiasnicolasruiz40@gmail.com>
2025-11-03 00:29:01 -0300

    practica estrc. de datos 1er ejrc.

---
+++ ultimos.ejerc/tiposlistas.py
@@ -0,0 +1,10 @@
+precios_frutas = {"Banana": 1200, "Anana": 2500, "Melon": 3000, "Uva": 1450}
+precios_frutas["Naranja"] = 1200
+precios_frutas["Manzana"] = 1500
+precios_frutas["Pera"] = 2300
+precios_frutas["Banana"] = 1330
+precios_frutas["Manzana"] = 1700
+precios_frutas["Melon"] = 2800
+listafrutas = list(precios_frutas.keys())
+print(f"\n{precios_frutas}")
+print(f"\n{listafrutas}")
